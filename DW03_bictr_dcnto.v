f16f91166729b585a2ca3695e87dd8e037e27b40
Gheorghe Lucian <115155585+ElTheScreeps@users.noreply.github.com>
2022-10-06 15:12:55 +0300

    Update DW03_bictr_dcnto.v

--- DW03_bictr_dcnto.v
+++ DW03_bictr_dcnto.v
@@ -3,14 +3,14 @@ module DW03_bictr_dcnto(data, up_dn, load, cen, clk, reset, count_to, count, ter
 parameter width = 8;
 
 input  [3:0] data;
-input 		 up_dn;
-input 		 load;
-input 		 cen;
-input 		 clk;
-input 		 reset;
+input 	     up_dn;
+input 	     load;
+input 	     cen;
+input 	     clk;
+input 	     reset;
 input  [3:0] count_to;
 output [3:0] count;
-output 		 tercnt;
+output 	     tercnt;
 
 reg    [3:0] count_intern;
 
