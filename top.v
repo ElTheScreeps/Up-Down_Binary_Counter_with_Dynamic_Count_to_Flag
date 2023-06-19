1267c1c36119af4bbbdba811f694e5e3b3a8f21c
Gheorghe Lucian <115155585+ElTheScreeps@users.noreply.github.com>
2023-06-19 09:34:13 +0300

    Update top.v

--- top.v
+++ top.v
@@ -1,15 +1,17 @@
 module top();
 
-reg 	   clk;
-reg 	   reset;
-reg 	   load;
-reg 	   cen;
-reg 	   up_dn;
-reg  [3:0] count_to;
-reg  [3:0] data;
+parameter width = 4;
 
-wire 	   tercnt;
-wire [3:0] count;
+reg 	         clk;
+reg 	         reset;
+reg 	         load;
+reg 	         cen;
+reg 	         up_dn;
+reg  [width-1:0] count_to;
+reg  [width-1:0] data;
+
+wire 	         tercnt;
+wire [width-1:0] count;
 
 always #5 clk <= ~clk;
 
@@ -19,18 +21,21 @@ reset 	 = 1;
 load  	 = 1;
 cen   	 = 0;
 up_dn 	 = 1;
-data 	 = 0;
+data 	 = 4'b1010;
 count_to = 4'b0100;
 repeat(1) @(posedge clk);
 reset 	 = 0;
 repeat(1) @(posedge clk);
 reset 	 = 1;
 load  	 = 0;
-repeat(3) @(posedge clk);
-reset 	 = 1;
-load  	 = 1;
-repeat(2) @(posedge clk);
+repeat(5) @(posedge clk);
 cen   	 = 1;
+load     = 1;
+repeat(15) @(posedge clk);
+up_dn    = 0;
+repeat(15) @(posedge clk);
+up_dn    = 1;
+load     = 0;
 repeat(15) @(posedge clk);
 end
 
