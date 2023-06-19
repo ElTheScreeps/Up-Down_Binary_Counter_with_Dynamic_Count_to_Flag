22b4f2a650d478b47b6aa7f3cc37a0661f826ce4
Gheorghe Lucian <115155585+ElTheScreeps@users.noreply.github.com>
2023-06-19 09:33:59 +0300

    Update DW03_bictr_dcnto.v

--- DW03_bictr_dcnto.v
+++ DW03_bictr_dcnto.v
@@ -1,28 +1,28 @@
 module DW03_bictr_dcnto(data, up_dn, load, cen, clk, reset, count_to, count, tercnt);
 
-parameter width = 8;
+parameter width = 4;
 
-input  [3:0] data;
-input 	     up_dn;
-input 	     load;
-input 	     cen;
-input 	     clk;
-input 	     reset;
-input  [3:0] count_to;
-output [3:0] count;
-output 	     tercnt;
+input  [width-1:0] data;
+input 	           up_dn;
+input 	           load;
+input 	           cen;
+input 	           clk;
+input 	           reset;
+input  [width-1:0] count_to;
+output [width-1:0] count;
+output 	           tercnt;
 
-reg    [3:0] count_intern;
+reg    [width-1:0] count_intern;
 
 always @(posedge(clk) or negedge(reset))
 begin
 	if (~reset) count_intern <= 'b0;
-	else if (cen == 0) count_intern <= count_intern;
-	else if (cen == 1)
+	else if (reset & load == 0) count_intern <= data;
+	else if (cen == 0 & load == 1) count_intern <= count_intern;
+	else if (cen == 1 & load == 1)
 	begin
-        	if (load == 1 & up_dn == 0) count_intern <= data;
-        	else if (up_dn == 1 & load == 0) count_intern <= count_intern + 1;
-         	else if (up_dn == 0 & load == 0) count_intern <= count_intern - 1;
+		if (up_dn) count_intern <= count_intern + 1;
+		else count_intern <= count_intern - 1;
 	end
 end
 		
