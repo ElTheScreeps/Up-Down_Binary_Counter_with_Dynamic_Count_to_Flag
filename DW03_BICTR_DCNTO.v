ee742997901bfc9639291a36a9bf21e97b38966f
Gheorghe Lucian <115155585+ElTheScreeps@users.noreply.github.com>
2023-06-21 20:06:18 +0300

    Add files via upload

---
+++ DW03_BICTR_DCNTO.v
@@ -0,0 +1,33 @@
+module DW03_BICTR_DCNTO(data, up_dn, load, cen, clk, reset, count_to, count, tercnt);
+
+parameter width = 4;
+
+input  [width-1:0] data;
+input 	           up_dn;
+input 	           load;
+input 	           cen;
+input 	           clk;
+input 	           reset;
+input  [width-1:0] count_to;
+output [width-1:0] count;
+output 	           tercnt;
+
+reg    [width-1:0] count_intern;
+
+always @(posedge(clk) or negedge(reset))
+begin
+	if (~reset) count_intern <= 'b0;
+	else if (reset & load == 0) count_intern <= data;
+	else if (cen == 0 & load == 1) count_intern <= count_intern;
+	else if (cen == 1 & load == 1)
+	begin
+		if (up_dn) count_intern <= count_intern + 1;
+		else count_intern <= count_intern - 1;
+	end
+end
+		
+assign count = count_intern;
+
+assign tercnt = (count_to == count_intern)? 1 : 0;
+
+endmodule
---
+++ DW03_BICTR_DCNTO_TB.v
@@ -0,0 +1,54 @@
+module DW03_BICTR_DCNTO_TB();
+
+parameter width = 4;
+
+reg 	         clk;
+reg 	         reset;
+reg 	         load;
+reg 	         cen;
+reg 	         up_dn;
+reg  [width-1:0] count_to;
+reg  [width-1:0] data;
+
+wire 	         tercnt;
+wire [width-1:0] count;
+
+always #5 clk <= ~clk;
+
+initial begin
+clk   	 = 0;
+reset 	 = 1;
+load  	 = 1;
+cen   	 = 0;
+up_dn 	 = 1;
+data 	 = 4'b1010;
+count_to = 4'b0100;
+repeat(1) @(posedge clk);
+reset 	 = 0;
+repeat(1) @(posedge clk);
+reset 	 = 1;
+load  	 = 0;
+repeat(5) @(posedge clk);
+cen   	 = 1;
+load     = 1;
+repeat(15) @(posedge clk);
+up_dn    = 0;
+repeat(15) @(posedge clk);
+up_dn    = 1;
+load     = 0;
+repeat(15) @(posedge clk);
+end
+
+DW03_bictr_dcnto #(width) T (
+.data(data),
+.count_to(count_to),
+.up_dn(up_dn),
+.load(load),
+.cen(cen),
+.clk(clk),
+.reset(reset),
+.count(count),
+.tercnt(tercnt)
+);
+
+endmodule
