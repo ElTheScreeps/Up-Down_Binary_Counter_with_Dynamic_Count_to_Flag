f238a2d99c29e4dbbfa7d8941d1346932c108b93
Gheorghe Lucian <115155585+ElTheScreeps@users.noreply.github.com>
2023-06-21 16:22:01 +0300

    Update top.v

--- top.v
+++ top.v
@@ -2,18 +2,17 @@ module top();
 
 parameter width = 4;
 
-reg 	         clk;
-reg 	         reset;
-reg 	         load;
-reg 	         cen;
+reg              clk;
+reg              reset;
+reg              load;
+reg              cen;
 reg 	         up_dn;
-reg  [width-1:0] count_to;
 reg  [width-1:0] data;
 
 wire 	         tercnt;
 wire [width-1:0] count;
 
-always #5 clk <= ~clk;
+always #5 clk = ~clk; 
 
 initial begin
 clk   	 = 0;
@@ -21,8 +20,7 @@ reset 	 = 1;
 load  	 = 1;
 cen   	 = 0;
 up_dn 	 = 1;
-data 	 = 4'b1010;
-count_to = 4'b0100;
+data 	 = 4'b0111;
 repeat(1) @(posedge clk);
 reset 	 = 0;
 repeat(1) @(posedge clk);
@@ -39,6 +37,15 @@ load     = 0;
 repeat(15) @(posedge clk);
 end
 
-DW03_bictr_dcnto #(8) U1 (.data(data), .count_to(count_to), .up_dn(up_dn), .load(load), .cen(cen), .clk(clk), .reset(reset), .count(count), .tercnt(tercnt));
+DW03_updn_ctr #(width) T (
+.data(data),
+.up_dn(up_dn),
+.load(load),
+.cen(cen),
+.clk(clk),
+.reset(reset),
+.count(count),
+.tercnt(tercnt)
+);
 
 endmodule
